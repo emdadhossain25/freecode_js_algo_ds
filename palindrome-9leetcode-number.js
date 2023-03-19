3ab663b81faafdd1f72e9fd1f4e6d676df933e29
emdad <emdad@ergo-ventures.com>
2023-03-19 16:38:19 +0600

    checked pallindrom problem

--- palindrome-9leetcode-number.js
+++ palindrome-9leetcode-number.js
@@ -4,10 +4,13 @@ let reverseNumber = 0;
 while (copy > 0) {
   let remainder = copy % 10;
   reverseNumber = reverseNumber * 10 + remainder;
-  copy = ~~(copy / 10); // divides and floors to
+  copy = Math.floor(copy / 10); // divides and floors to
 }
+console.log(reverseNumber);
+console.log(original);
 if (reverseNumber === original) {
   return true;
 } else {
   return false;
 }
+
